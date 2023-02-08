c89df4a9a82eb65250f0a42806ef3855f154a01b
Patrice-H <webmestre64@gmail.com>
2023-02-08 22:48:41 +0100

    Template contact-form

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -8,21 +8,33 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ContactFormComponent implements OnInit {
   formContainer!: FormGroup;
+  eMailRegex!: RegExp;
+  formErrors!: Boolean;
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.eMailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     this.formContainer = this.formBuilder.group({
       firstName: [null],
       lastName: [null],
       companyName: [null],
-      eMail: [null],
+      eMail: ['', [Validators.required, Validators.pattern(this.eMailRegex)]],
       subject: [null],
       message: [null],
     });
+    this.formErrors = false;
   }
 
   onSubmitForm(): void {
     console.log(this.formContainer.value);
+  }
+
+  isConform(): boolean {
+    if (Validators.pattern(this.eMailRegex)) {
+      return true;
+    }
+
+    return false;
   }
 }
